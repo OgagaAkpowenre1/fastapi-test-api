1099e045925869a4442905e0a9618a0a26c89d72
ogagaogheneakpowenre <ogagaogheneakpowenre@gmail.com>
2025-06-11 03:27:13 +0000

    support for mobile screens started; menu toggling added; to do: swap components based on toggling

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,10 +1,12 @@
 import './navbar.css'
 
-const Navbar = () => {
+const Navbar = ({toggleNoteList, showNoteList}) => {
     return (
         <nav className='navbar'>
             <a href="#"><h1>Note Taker</h1></a>
-            <div className="user-info">
+            <div className="right-items">
+            {!showNoteList && <i class="fa-solid fa-bars" onClick={toggleNoteList}></i>}
+            {showNoteList && <i class="fa-solid fa-xmark" onClick={toggleNoteList}></i>}
                 <p>username</p>
                 <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.freeiconspng.com%2Fuploads%2Fperson-icon-5.png&f=1&nofb=1&ipt=3f2d9b0425ff691815e112ec38ebda4651311e40ba19208cdbfd4f6cc6b7a993" alt="" />
             </div>
--- frontend/src/components/noteAppContainer/noteAppContainer.jsx
+++ frontend/src/components/noteAppContainer/noteAppContainer.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import './noteAppContainer.css'
 import NoteList from '../noteList/noteList';
 import TextBox from '../textBox/textBox';
 import Navbar from '../navbar/navbar'
 
 const NoteAppContainer = () => {
+    const [showNoteList, setShowNoteList] = useState(false)
+    
     return (
         <>
-            <Navbar />
+            <Navbar toggleNoteList={() => {setShowNoteList(!showNoteList);console.log(showNoteList)}} showNoteList={showNoteList} />
             <div className='note-app-container'>
             <TextBox />
             <NoteList />
